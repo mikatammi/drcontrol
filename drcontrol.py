36083bddcc7748fbd9917b64f954eadc1b7c9446
Mika Tammi <mika.tammi@unikie.com>
2023-03-15 19:12:23 +0200

    Remove a useless check
    This check is useless with Nix, where the pylibftdi dependency takes
    care of loading libftdi in runtime
    
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

--- drcontrol.py
+++ drcontrol.py
@@ -220,12 +220,6 @@ def check():
         print("Error: Your Python need to be 3.4 or newer")
         sys.exit(1)
 
-    #Check availability on library, this check is also done in pylibftdi
-    ftdi_lib = find_library('libftdi1')
-    if ftdi_lib is None:
-       print("Error: libftdi library not found")
-       sys.exit(1)
-
 if __name__ == '__main__':
 
     # Init objects
