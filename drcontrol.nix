2333724d23f2d24c052153a99f3268f1c7978422
Mika Tammi <mika.tammi@unikie.com>
2023-12-02 05:26:51 +0200

    Fix build on NixOS 23.11
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

--- drcontrol.nix
+++ drcontrol.nix
@@ -1,14 +1,24 @@
 {
-  lib,
   python3Packages,
-  libftdi,
-}:
-python3Packages.buildPythonApplication {
-  pname = "drcontrol";
-  version = "0.13";
-  propagatedBuildInputs = [python3Packages.pylibftdi];
-  src = ./.;
-  postFixup = ''
-    mv $out/bin/drcontrol.py $out/bin/drcontrol
-  '';
-}
+  libusb1,
+  libftdi1,
+}: let
+  pylibftdiWithOverrides = python3Packages.pylibftdi.overrideAttrs (_: {
+    postPatch = ''
+      substituteInPlace src/pylibftdi/driver.py \
+        --replace "self._load_library(\"libusb\")" "cdll.LoadLibrary('${libusb1.out}/lib/libusb-1.0.so')" \
+        --replace "self._load_library(\"libftdi\")" "cdll.LoadLibrary('${libftdi1.out}/lib/libftdi1.so')"
+    '';
+  });
+in
+  python3Packages.buildPythonApplication {
+    pname = "drcontrol";
+    version = "0.13";
+    propagatedBuildInputs = [
+      pylibftdiWithOverrides
+    ];
+    src = ./.;
+    postFixup = ''
+      mv $out/bin/drcontrol.py $out/bin/drcontrol
+    '';
+  }
